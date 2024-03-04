24623a403194b35a97782e9d7aa83566e8b6bd06
icestorm8 <shaked5001@gmail.com>
2024-03-04 14:50:44 +0200

    created a footer elment using react icons ( added to dependencies ) - need to add links to git and linkedin

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+export default function Footer() {
+  return (
+    <footer className="pt-3 bg-secondary bg-gradient text-white text-center">
+      {/* footer content will be here */}
+      <div className="row">
+        <div className="col-12 ">
+          <p className="small">shaked tamam</p>
+          <p className="small">2024</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 display-6 ">
+          {/* github */}
+          <a className="icon-link p-3" href="#!" role="button">
+            <FaGithub className="text-white"></FaGithub>
+          </a>
+          {/* linkedin */}
+          <a className="icon-link p-3" href="#!" role="button">
+            <FaLinkedin className="text-white"></FaLinkedin>
+          </a>
+        </div>
+      </div>
+
+      <div className="bg-dark">
+        <p className="p-2">&copy; all rights reserved to shaked tamam</p>
+      </div>
+    </footer>
+  );
+}
